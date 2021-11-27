575b061461619eea9564d0e9525ef4931df8d2a8
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-11-27 16:35:58 -0600

    create - vistas de teachers

--- app/Http/Controllers/TeacherConctroller.php
+++ app/Http/Controllers/TeacherConctroller.php
@@ -2,19 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Teacher;
 use Illuminate\Http\Request;
+use App\Models\University;
+use App\Models\Department;
+use App\Models\Center;
 
 class TeacherConctroller extends Controller
 {
 
     public function index()
     {
-        //
+        $teachers = Teacher::paginate(5);
+        return view('teachers.teacherIndex', compact('teachers'));
     }
 
     public function create()
     {
-        //
+        $universities = University::all();
+        $departments  = Department::all();
+        $centers      = Center::all();
+
+        return view('teachers.teacherCreate', compact('universities', 'centers', 'centers'));
     }
 
     public function store(Request $request)
@@ -22,24 +31,25 @@ class TeacherConctroller extends Controller
         //
     }
 
-    public function show($id)
+    public function show($teacher)
     {
         //
     }
 
-    public function edit($id)
+    public function edit($teacher)
     {
         //
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $teacher)
     {
         //
     }
 
-    public function destroy($id)
+    public function destroy(Teacher $teacher)
     {
-        //
+        $teacher->delete();
+        return redirect()->route('teachers.index')->with('info', 'Profesor eliminado correctamente');
     }
-    
+
 }
--- app/Models/Center.php
+++ app/Models/Center.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Teacher;
 
 class Center extends Model
 {
     use HasFactory;
+
+    public function teachers() {
+        return $this->hasMany(Teacher::class, 'teacher_id');
+    }
+
 }
