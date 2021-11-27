12d0df0a2b7feb3912ddcff190025bcc99604aed
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-11-27 11:16:14 -0600

    Estilos bootstrap 5 y navbar

---
+++ resources/views/partials/template.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--  Iconos Bootstrap  --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    {{--  Fuentes de letra  --}}
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+
+    <div id="app">
+
+
+        {{--  Navbar  --}}
+        @include('partials.navbar')
+
+        {{--  Content  --}}
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+
+    </div>
+</body>
+</html>
