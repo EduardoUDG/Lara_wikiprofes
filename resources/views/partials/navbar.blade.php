37a731f79df929f8b641229fdb8a7fb4fd853eec
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-11-27 11:27:54 -0600

    update - navbar with style bootstrap 5 and align

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,15 +1,19 @@
-<nav class="navbar navbar-expand-md navbar-light bg-light shadow">
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
     <div class="container">
-        <a class="navbar-brand font-weight-bold" href="{{ url('/') }}">
+
+        {{--  Logo  --}}
+        <a class="navbar-brand fw-bold" href="{{ url('/') }}">
             Wikiprofes2.0
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
+        {{-- Menu links --}}
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
+            {{--  Links  --}}
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" href="#">Búsqueda</a>
                 </li>
@@ -24,41 +28,45 @@
                 </li>
             </ul>
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item mx-3">
-                            <a class="btn btn-primary px-3" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
-                        </li>
-                    @endif
+            {{--  Login/Register  --}}
+            <div class="d-flex gap-3">
+                <ul class="navbar-nav ml-auto">
+                    {{--  Authentication Links  --}}
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item mx-3">
+                                <a class="btn btn-primary px-3" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
+                            </li>
+                        @endif
 
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="btn btn-outline-primary px-3" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="btn btn-outline-primary px-3"
+                                    href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
                             </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
 
     <div class="container">
         <h1>Wikiprofes</h1>
-        <button class="btn btn-primary">Boton primary</button>
     </div>
 
 @endsection
