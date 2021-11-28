fd9d0d52050d03cb4f61d0e158c206e6774a19ed
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-11-27 19:20:44 -0600

    update - restore columns of migrations and methods

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,15 +11,15 @@ class Comment extends Model
 
 
     public function teacher() {
-        return $this->belongsTo(Teacher::class, 'id');
+        return $this->belongsTo(Teacher::class, 'teacher_id');
     }
 
     public function user() {
-        return $this->belongsTo(User::class, 'id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function subject() {
-        return $this->belongsTo(Subject::class, 'id');
+        return $this->belongsTo(Subject::class, 'subject_id');
     }
 
 
